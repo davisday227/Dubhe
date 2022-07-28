770aa28c29f550ad665096b46fa0f7080061d408
davisday227 <davisday227@gmail.com>
2022-07-28 23:06:06 +0800

    dynamic delete route

--- gateway/src/main/java/com/panerai/gateway/route/controller/RouteController.java
+++ gateway/src/main/java/com/panerai/gateway/route/controller/RouteController.java
@@ -1,8 +1,15 @@
 package com.panerai.gateway.route.controller;
 
+import com.panerai.gateway.route.entity.GatewayDefine;
+import com.panerai.gateway.route.service.GatewayDefineService;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.InMemoryRouteDefinitionRepository;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -10,12 +17,31 @@ import reactor.core.publisher.Flux;
 @RestController
 @RequestMapping("/route")
 public class RouteController {
-    @Autowired
-    private RouteDefinitionLocator routeDefinitionLocator;
 
-    //获取网关所有的路由信息
-    @RequestMapping("/routes")
-    public Flux<RouteDefinition> getRouteDefinitions() {
-        return routeDefinitionLocator.getRouteDefinitions();
-    }
+  @Autowired
+  private InMemoryRouteDefinitionRepository routeDefinitionLocator;
+
+  @Autowired
+  private GatewayDefineService defineService;
+
+  @PostMapping("/routes")
+  public void addRoutes() {
+    GatewayDefine define = new GatewayDefine();
+    define.setId(RandomStringUtils.randomAlphabetic(6));
+    define.setUri("http://localhost:8091");
+    define.setPredicates("Path:/authors/**");
+    define.setTimeout(RandomUtils.nextInt(10, 100));
+    defineService.addSingleRoute(define);
+  }
+
+  @GetMapping("/deleteAll")
+  public void deleteAll() {
+    defineService.deleteAll();
+  }
+
+  //获取网关所有的路由信息
+  @RequestMapping("/routes")
+  public Flux<RouteDefinition> getRouteDefinitions() {
+    return routeDefinitionLocator.getRouteDefinitions();
+  }
 }
--- gateway/src/main/java/com/panerai/gateway/route/service/GatewayDefineService.java
+++ gateway/src/main/java/com/panerai/gateway/route/service/GatewayDefineService.java
@@ -6,80 +6,104 @@ import static org.springframework.cloud.gateway.support.RouteMetadataUtils.RESPO
 import com.panerai.gateway.route.entity.GatewayDefine;
 import com.panerai.gateway.route.repository.GatewayDefineRepository;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.apache.commons.collections4.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
 import org.springframework.cloud.gateway.filter.FilterDefinition;
 import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
+import org.springframework.cloud.gateway.route.InMemoryRouteDefinitionRepository;
 import org.springframework.cloud.gateway.route.RouteDefinition;
-import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.gateway.support.NotFoundException;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
 
 @Service
 public class GatewayDefineService implements ApplicationEventPublisherAware {
-    @Autowired
-    private GatewayDefineRepository gatewayDefineRepository;
 
-    @Autowired
-    private RouteDefinitionWriter routeDefinitionWriter;
+  private static Logger log = LoggerFactory.getLogger(GatewayDefineService.class);
 
-    private ApplicationEventPublisher publisher;
+  @Autowired
+  private GatewayDefineRepository gatewayDefineRepository;
 
-    public List<GatewayDefine> findAll() {
-        return gatewayDefineRepository.findAll();
+  @Autowired
+  private InMemoryRouteDefinitionRepository routeDefinitionWriter;
+
+  private ApplicationEventPublisher publisher;
+
+  public String loadRouteDefinition() {
+    List<GatewayDefine> gatewayDefineList = gatewayDefineRepository.findAll();
+
+    if (CollectionUtils.isEmpty(gatewayDefineList)) {
+      throw new RuntimeException("没有任何配置，无法启动");
     }
 
-    public String loadRouteDefinition() {
-        List<GatewayDefine> gatewayDefineList = findAll();
+    try {
+      for (GatewayDefine define : gatewayDefineList) {
+        addRouteDefinition(define);
+      }
+      this.publisher.publishEvent(new RefreshRoutesEvent(this));
+      return "success";
+    } catch (Exception e) {
+      log.info("something wrong", e);
+      return "failure";
+    }
+  }
 
-        if (CollectionUtils.isEmpty(gatewayDefineList)) {
-            throw new RuntimeException("没有任何配置，无法启动");
-        }
+  public void addSingleRoute(GatewayDefine define) {
+    addRouteDefinition(define);
 
-        try {
-            for (GatewayDefine define : gatewayDefineList) {
-                RouteDefinition definition = new RouteDefinition();
+    this.publisher.publishEvent(new RefreshRoutesEvent(this));
+  }
 
-                definition.setId(define.getId());
-                definition.setUri(new URI(define.getUri()));
-                List<PredicateDefinition> predicateDefinitions = define.getPredicateDefinition();
-                if (CollectionUtils.isEmpty(predicateDefinitions)) {
-                    throw new RuntimeException("predicate must not be empty");
-                }
+  private void addRouteDefinition(GatewayDefine define) {
+    RouteDefinition definition = new RouteDefinition();
 
-                Map<String, Object> timeoutMap = new HashMap<>();
-                timeoutMap.put(RESPONSE_TIMEOUT_ATTR, define.getTimeout() * 1000);
-                timeoutMap.put(CONNECT_TIMEOUT_ATTR, define.getTimeout() * 1000);
-                definition.setMetadata(timeoutMap);
-                PredicateDefinition methodPredicate = define.buildMethodPredicate();
-                if (methodPredicate != null) {
-                    predicateDefinitions.add(methodPredicate);
-                }
-                definition.setPredicates(predicateDefinitions);
-                List<FilterDefinition> filterDefinitions = define.getFilterDefinition();
-                if (CollectionUtils.isNotEmpty(filterDefinitions)) {
-                    definition.setFilters(filterDefinitions);
-                }
-                routeDefinitionWriter.save(Mono.just(definition)).subscribe();
-            }
-            this.publisher.publishEvent(new RefreshRoutesEvent(this));
-            return "success";
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            return "failure";
-        }
+    definition.setId(define.getId());
+    definition.setUri(UriComponentsBuilder.fromUriString(define.getUri()).build().toUri());
+    List<PredicateDefinition> predicateDefinitions = define.getPredicateDefinition();
+    if (CollectionUtils.isEmpty(predicateDefinitions)) {
+      throw new RuntimeException("predicate must not be empty");
     }
 
-    @Override
-    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
-        publisher = applicationEventPublisher;
+    Map<String, Object> timeoutMap = new HashMap<>();
+    timeoutMap.put(RESPONSE_TIMEOUT_ATTR, define.getTimeout() * 1000);
+    timeoutMap.put(CONNECT_TIMEOUT_ATTR, define.getTimeout() * 1000);
+    definition.setMetadata(timeoutMap);
+    PredicateDefinition methodPredicate = define.buildMethodPredicate();
+    if (methodPredicate != null) {
+      predicateDefinitions.add(methodPredicate);
     }
+    definition.setPredicates(predicateDefinitions);
+    List<FilterDefinition> filterDefinitions = define.getFilterDefinition();
+    if (CollectionUtils.isNotEmpty(filterDefinitions)) {
+      definition.setFilters(filterDefinitions);
+    }
+    routeDefinitionWriter.save(Mono.just(definition)).subscribe();
+  }
+
+  @Override
+  public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+    publisher = applicationEventPublisher;
+  }
+
+  public void deleteAll() {
+    routeDefinitionWriter.getRouteDefinitions()
+        .subscribe(route -> {
+          routeDefinitionWriter.delete(Mono.just(route.getId())).subscribe();
+          log.info("delete route: {}.", route.getId());
+        });
+
+//    routeDefinitionWriter.delete(Mono.just("1")).subscribe();
+
+    this.publisher.publishEvent(new RefreshRoutesEvent(this));
+  }
 }

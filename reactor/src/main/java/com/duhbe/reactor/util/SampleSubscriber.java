58ae9f0f4ec3abd87d687dbe57524a8d420a753a
davisday227 <davisday227@gmail.com>
2022-07-31 22:12:36 +0800

    add reactor example

---
+++ reactor/src/main/java/com/duhbe/reactor/util/SampleSubscriber.java
@@ -0,0 +1,26 @@
+package com.duhbe.reactor.util;
+
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+public class SampleSubscriber<T> extends BaseSubscriber<T> {
+
+  @Override
+  protected void hookOnSubscribe(Subscription subscription) {
+    System.out.println("subscribed");
+    request(1);
+  }
+
+  @Override
+  protected void hookOnNext(T value) {
+    System.out.println(value);
+    request(1);
+  }
+
+  public static void main(String[] args) {
+    SampleSubscriber<Integer> ss = new SampleSubscriber<>();
+    Flux<Integer> ints = Flux.range(1, 5);
+    ints.subscribe(ss);
+  }
+}
